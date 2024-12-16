4e9de047841abb019cb9d410c45e0c2ca0d1c2a8
Robert Sanins <robertsanins@gmail.com>
2024-12-16 15:44:45 +0000

    feat: create basic layout (header, footer)

---
+++ client/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+        <div>
+            <img src="/octopus-logo.svg" alt="Octopus Energy Logo" />
+            <button>
+            <img src="/basket.svg" alt="Basket Icon" />
+            </button>
+        </div>
+        </header>
+    );
+};
+  
+export default Header;
